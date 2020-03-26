c4a85de8751db2c3b777cc2a2f8e47c6fddcf5f6
pziubinski <p.ziubinski@gmail.com>
2020-03-26 11:00:14 +0100

    Zadanie 19.2.2

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,22 +1,31 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/task")
 public class TaskController {
-    @RequestMapping(method = RequestMethod.GET, value = "getTask")
-    public List<TaskDto> getTask() {
-        return new ArrayList<>();
-    }
+    @Autowired
+    private DbService service;
+
+    @Autowired
+    private TaskMapper taskMapper;
 
     @RequestMapping(method = RequestMethod.GET, value = "getTasks")
+    public List<TaskDto> getTask() {
+        return taskMapper.mapToTaskDtoList(service.getAllTasks());
+    }
+
+/*
+    @RequestMapping(method = RequestMethod.GET, value = "getTask")
     public TaskDto getTask(Long taskId) {
         return new TaskDto(1L, "test title", " test content");
     }
@@ -35,4 +44,6 @@ public class TaskController {
     public void createTask(TaskDto task) {
 
     }
+
+ */
 }
