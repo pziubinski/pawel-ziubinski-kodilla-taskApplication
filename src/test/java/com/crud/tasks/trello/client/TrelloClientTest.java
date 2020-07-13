27616205576b9f7e94ce125aa97e3f9ed24072e3
ziubi <p.ziubinski@gmail.com>
2020-07-13 23:21:27 +0200

    Zadanie 29.2

--- src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
+++ src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
@@ -35,6 +35,7 @@ public class TrelloClientTest {
         when(trelloConfig.getTrelloApiEndpoint()).thenReturn("http://test.com");
         when(trelloConfig.getTrelloAppKey()).thenReturn("test");
         when(trelloConfig.getTrelloToken()).thenReturn("test");
+        when(trelloConfig.getTrelloUserName()).thenReturn("paweziubinski");
     }
 
     @Test
